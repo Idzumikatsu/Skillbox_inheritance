dddfa91a5e988f9a21bd6ca2c7593606d2ff3c75
k0na <pwpvpdru@yandex.ru>
2023-06-20 21:56:12 +0300

    lesson_13.8 homework done - add new methods in BankAccount.java - overrides methods from super to CardAccount.java and DepositAccount.java - set access to variables and methods

--- src/main/java/practice/DepositAccount.java
+++ src/main/java/practice/DepositAccount.java
@@ -1,5 +1,33 @@
 package practice;
 
+import java.time.LocalDate;
+
 public class DepositAccount extends BankAccount {
+
+    private LocalDate lastIncome;
+
+    public DepositAccount() {
+        super();
+    }
+
+    @Override
+    public double getAmount() {
+        return super.getAmount();
+    }
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        int isOneMonthLeft = LocalDate.now().compareTo(lastIncome.plusMonths(1));
+        if (isOneMonthLeft > 0){
+            super.take(amountToTake);
+        }
+    }
+
 
 }
